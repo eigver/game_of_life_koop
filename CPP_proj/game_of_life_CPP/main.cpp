f0e90b3ac5149503ff15307e79281b2316985a4d
eigver <work.eigver@gmail.com>
2025-07-04 13:36:04 +0300

    now based on raylib

--- CPP_proj/game_of_life_CPP/main.cpp
+++ CPP_proj/game_of_life_CPP/main.cpp
@@ -1,21 +1,18 @@
-#include <SFML/Graphics.hpp>
+#include "raylib.h"
 
-int main()
+int main(void)
 {
-    sf::RenderWindow window(sf::VideoMode({ 200, 200 }), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    InitWindow(800, 450, "raylib [core] example - basic window");
 
-    while (window.isOpen())
+    while (!WindowShouldClose())
     {
-        while (const std::optional event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
-                window.close();
-        }
-
-        window.clear();
-        window.draw(shape);
-        window.display();
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        EndDrawing();
     }
+
+    CloseWindow();
+
+    return 0;
 }
